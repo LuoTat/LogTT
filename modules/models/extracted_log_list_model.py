0d086aba62c2137842db5f7bb8a5d7acad40ce19
LuoTat <LuoTat.s@gmail.com>
2026-01-27 21:48:00 +0800

    用来显示可选的已提取的日志的列表，在查看结构化日志和模板的页面有使用

---
+++ modules/models/extracted_log_list_model.py
@@ -0,0 +1,86 @@
+from typing import Any
+from pathlib import Path
+from enum import IntEnum
+
+from PySide6.QtCore import (
+    Qt,
+    QModelIndex,
+    QAbstractListModel
+)
+
+from .duckdb_service import DuckDBService
+
+
+class SqlColumn(IntEnum):
+    """日志表数据库列枚举"""
+
+    ID = 0  # id
+    LOG_URI = 1  # log_uri
+    LOG_STRUCTURED = 2  # log_structured
+    LOG_TEMPLATES = 3  # log_templates
+
+
+class ExtractedLogListModel(QAbstractListModel):
+    """已提取日志的模型类"""
+
+    # 用户自定义角色
+    LOG_ID_ROLE = Qt.ItemDataRole.UserRole + 1  # 日志ID
+    LOG_STRUCTURED_PATH_ROLE = Qt.ItemDataRole.UserRole + 2  # 结构化日志路径
+    LOG_TEMPLATES_PATH_ROLE = Qt.ItemDataRole.UserRole + 3  # 模板日志路径
+    STRUCTURED_TABLE_NAME_ROLE = Qt.ItemDataRole.UserRole + 4  # 结构化表名
+    TEMPLATES_TABLE_NAME_ROLE = Qt.ItemDataRole.UserRole + 5  # 模板表名
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self._duckdb_service = DuckDBService()
+        # 一次性获取整个表的数据到内存中
+        self._df: list[tuple] = self._duckdb_service.get_extracted_log_table()
+
+    # ==================== 重写方法 ====================
+
+    def rowCount(self, parent: QModelIndex = QModelIndex()) -> int:
+        return len(self._df)
+
+    def data(self, index: QModelIndex, role: int = Qt.ItemDataRole.DisplayRole) -> Any:
+        if not index.isValid():
+            return None
+
+        row = index.row()
+
+        # 显示角色
+        if role in (Qt.ItemDataRole.DisplayRole, Qt.ItemDataRole.EditRole):
+            return self._df[row][SqlColumn.LOG_URI]
+
+        # 自定义角色：返回各种数据
+        elif role == self.LOG_ID_ROLE:
+            return int(self._df[row][SqlColumn.ID])
+
+        elif role == self.LOG_STRUCTURED_PATH_ROLE:
+            return self._df[row][SqlColumn.LOG_STRUCTURED]
+
+        elif role == self.LOG_TEMPLATES_PATH_ROLE:
+            return self._df[row][SqlColumn.LOG_TEMPLATES]
+
+        elif role == self.STRUCTURED_TABLE_NAME_ROLE:
+            return Path(self._df[row][SqlColumn.LOG_STRUCTURED]).stem
+
+        elif role == self.TEMPLATES_TABLE_NAME_ROLE:
+            return Path(self._df[row][SqlColumn.LOG_TEMPLATES]).stem
+
+        return None
+
+    # ==================== 公共方法 ====================
+
+    def getRow(self, log_id: int) -> int | None:
+        """根据 log_id 获取行号"""
+        for idx, row in enumerate(self._df):
+            if row[SqlColumn.ID] == log_id:
+                return idx
+        return None
+
+    def refresh(self):
+        """刷新数据"""
+        self.beginResetModel()
+        self._df = self._duckdb_service.get_extracted_log_table()
+        self.endResetModel()
