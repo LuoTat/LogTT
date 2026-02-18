52983412febae5bab4b67fa419273e61fd7024a8
LuoTat <LuoTat.s@gmail.com>
2026-02-18 17:30:14 +0800

    把日志格式列表移动了一下

--- modules/models/format_type_list_model.py
+++ modules/models/format_type_list_model.py
@@ -3,98 +3,7 @@ from typing import Any
 from PySide6.QtCore import QAbstractListModel, QModelIndex, Qt
 
 from modules.app_config import appcfg
-
-buildin_log_formats = [
-    (
-        "HDFS",
-        "<Date> <Time> <Pid> <Level> <Component>: <Content>",
-        [r"blk_-?\d+", r"(\d+\.){3}\d+(:\d+)?"],
-    ),
-    (
-        "Hadoop",
-        r"<Date> <Time> <Level> \[<Process>\] <Component>: <Content>",
-        [r"(\d+\.){3}\d+"],
-    ),
-    (
-        "Spark",
-        "<Date> <Time> <Level> <Component>: <Content>",
-        [r"(\d+\.){3}\d+", r"\b[KGTM]?B\b", r"([\w-]+\.){2,}[\w-]+"],
-    ),
-    (
-        "Zookeeper",
-        r"<Date> <Time> - <Level>  \[<Node>:<Component>@<Id>\] - <Content>",
-        [r"(/|)(\d+\.){3}\d+(:\d+)?"],
-    ),
-    (
-        "BGL",
-        "<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>",
-        [r"core\.\d+", r"\d+:[a-fA-F0-9]{8,}", r"[a-fA-F0-9]{8,}"],
-    ),
-    (
-        "HPC",
-        "<LogId> <Node> <Component> <State> <Time> <Flag> <Content>",
-        [r"=\d+"],
-    ),
-    (
-        "Thunderbird",
-        r"<Label> <Timestamp> <Date> <User> <Month> <Day> <Time> <Location> <Component>(\[<PID>\])?: <Content>",
-        [r"(\d+\.){3}\d+"],
-    ),
-    (
-        "Windows",
-        "<Date> <Time>, <Level>                  <Component>    <Content>",
-        [r"0x.*?\s"],
-    ),
-    (
-        "Linux",
-        r"<Month> <Date> <Time> <Level> <Component>(\[<PID>\])?: <Content>",
-        [r"(\d+\.){3}\d+", r"\d{2}:\d{2}:\d{2}"],
-    ),
-    (
-        "Android",
-        "<Date> <Time>  <Pid>  <Tid> <Level> <Component>: <Content>",
-        [
-            r"(/[\w-]+)+",
-            r"([\w-]+\.){2,}[\w-]+",
-            r"\b(\-?\+?\d+)\b|\b0[Xx][a-fA-F\d]+\b|\b[a-fA-F\d]{4,}\b",
-        ],
-    ),
-    (
-        "HealthApp",
-        r"<Time>\|<Component>\|<Pid>\|<Content>",
-        [r"\d+##\d+##\d+##\d+##\d+##\d+", r"=\d+"],
-    ),
-    (
-        "Apache",
-        r"\[<Time>\] \[<Level>\] <Content>",
-        [r"(\d+\.){3}\d+"],
-    ),
-    (
-        "Proxifier",
-        r"\[<Time>\] <Program> - <Content>",
-        [
-            r"<\d+\ssec",
-            r"([\w-]+\.)+[\w-]+(:\d+)?",
-            r"\d{2}:\d{2}(:\d{2})*",
-            r"[KGTM]B",
-        ],
-    ),
-    (
-        "OpenSSH",
-        r"<Date> <Day> <Time> <Component> sshd\[<Pid>\]: <Content>",
-        [r"(\d+\.){3}\d+", r"([\w-]+\.){2,}[\w-]+"],
-    ),
-    (
-        "OpenStack",
-        r"<Logrecord> <Date> <Time> <Pid> <Level> <Component> \[<ADDR>\] <Content>",
-        [r"\[instance:\s*(.*?)\]", r"((\d+\.){3}\d+,?)+", r"/.+?\s", r"\d+"],
-    ),
-    (
-        "Mac",
-        r"<Month>  <Date> <Time> <User> <Component>\[<PID>\]( \(<Address>\))?: <Content>",
-        [r"([\w-]+\.){2,}[\w-]+"],
-    ),
-]
+from modules.constants import BUILTIN_LOG_FORMATS
 
 
 class FormatTypeListModel(QAbstractListModel):
@@ -102,12 +11,13 @@ class FormatTypeListModel(QAbstractListModel):
 
     # 用户自定义角色
     LOG_FORMAT_ROLE = Qt.ItemDataRole.UserRole + 1
-    LOG_FORMAT_REGEX_ROLE = Qt.ItemDataRole.UserRole + 2
+    LOG_FORMAT_MASK_ROLE = Qt.ItemDataRole.UserRole + 2
+    LOG_FORMAT_DELIM_ROLE = Qt.ItemDataRole.UserRole + 3
 
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self._df: list[tuple[str, str, list[str]]] = buildin_log_formats
+        self._df = BUILTIN_LOG_FORMATS
         if user_formats := appcfg.get(appcfg.userFormatType):
             self._df.append(user_formats)
 
@@ -130,7 +40,10 @@ class FormatTypeListModel(QAbstractListModel):
         elif role == self.LOG_FORMAT_ROLE:
             return self._df[row][1]
 
-        elif role == self.LOG_FORMAT_REGEX_ROLE:
+        elif role == self.LOG_FORMAT_MASK_ROLE:
             return self._df[row][2]
+
+        elif role == self.LOG_FORMAT_DELIM_ROLE:
+            return self._df[row][3]
 
         return None
