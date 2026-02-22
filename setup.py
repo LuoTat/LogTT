6e5005887410a7e7b074dd94f615fb1f35af586c
LuoTat <LuoTat.s@gmail.com>
2026-02-23 03:30:37 +0800

    添加了对msvc编译器的特殊处理

--- setup.py
+++ setup.py
@@ -1,26 +1,42 @@
 from Cython.Build import cythonize
 from Cython.Compiler import Options
-from setuptools import Extension, setup
+from setuptools import setup
+from setuptools.command.build_ext import build_ext
 
 Options.docstrings = False
 Options.fast_fail = True
 
-extensions = [
-    Extension(
-        "modules.logparser.*",
-        ["modules/logparser/*.pyx"],
-        extra_compile_args=[
-            "-DCYTHON_CLINE_IN_TRACEBACK=0",
-            "-O3",
-            "-std=c++20",
-        ],
-    ),
-]
+
+class BuildExtWithCompilerFlags(build_ext):
+    def build_extensions(self):
+        compiler_type = self.compiler.compiler_type
+
+        if compiler_type == "msvc":
+            compile_args = [
+                "/DCYTHON_CLINE_IN_TRACEBACK=0",
+                "/W4",
+                "/O2",
+                "/std:c++20",
+            ]
+        else:
+            compile_args = [
+                "-DCYTHON_CLINE_IN_TRACEBACK=0",
+                "-Wall",
+                "-O3",
+                "-std=c++20",
+            ]
+
+        for extension in self.extensions:
+            extension.extra_compile_args = compile_args
+
+        super().build_extensions()
+
 
 setup(
     packages=[],
+    cmdclass={"build_ext": BuildExtWithCompilerFlags},
     ext_modules=cythonize(
-        extensions,
+        "modules/logparser/*.pyx",
         force=True,
         show_all_warnings=True,
         annotate=True,
